31fdc013a47c04bf10784afc423281d2d5e5c90b
Mohammad Nouraein <MohammadNouraein@gmail.com>
2019-05-28 18:07:47 +0430

    test cases seperated

--- src/test.js
+++ src/test.js
@@ -1,11 +1,18 @@
 import assert from "assert";
 import Client from "./client/client";
 describe("Random Generator Client", () => {
-  it("Server must return exactly 6 random numbers (1-100)", function(done) {
+  it("Server must return exactly 6 random numbers", function(done) {
     this.timeout(10000);
     let client = new Client();
     client.getNewRandomNumberFromServer().then(res => {
       assert.equal(res.length, 6);
+      done();
+    });
+  });
+  it("All 6 random numbers must be between 0 and 100", function(done) {
+    this.timeout(10000);
+    let client = new Client();
+    client.getNewRandomNumberFromServer().then(res => {
       assert.equal(
         true,
         res &&
