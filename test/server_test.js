64cbdcb514587e00d6d1ef0e3c65af81bdaaa0a5
Mohammad Nouraein <MohammadNouraein@gmail.com>
2019-05-29 11:26:13 +0430

    test updates

--- test/server_test.js
+++ test/server_test.js
@@ -2,38 +2,27 @@ var expect = require("chai").expect;
 var assert = require("chai").assert;
 var client = require("../dist/client/client").default;
 
-/**
- * Random Generator Client
- */
 describe("Random Generator Client", function() {
+
   before(async function(){
     this.timeout(10000);
      clientApp = new client();
      res=await clientApp.getNewRandomNumberFromServer();
   });
 
-  /**
-   * The result must be an array
-   */
-  it("The result must be anon empty array",function(){
-    expect(res && Array.isArray(res)).to.equal(true);
+  it("The result must be a non empty array",function(){
+    expect(res && Array.isArray(res) && res.length).to.be.ok;
   })
 
-  /**
-   * Server must return exactly 6 random numbers
-   */
-  it("Server must return exactly 6 random numbers", function() {
+  it("Server must return exactly 6 item", function() {
       expect(res.length).to.equal(6);
   });
 
-  /**
-   * All 6 random numbers must be between 0 and 100
-   */
   it("All 6 random numbers must be between 0 and 100", function() {
       expect(
         res &&
           res.length &&
-          res.map(i => i <= 100 && i >= 0).indexOf(false) == -1
+          res.map(i =>typeof(i)=='number' && i <= 100 && i >= 0).indexOf(false) == -1
       ).to.equal(true);
   });
   
